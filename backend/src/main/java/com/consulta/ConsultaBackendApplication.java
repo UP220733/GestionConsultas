15306be0699c6daab2ed55e737b4a6fe8cd61475
Diego Sebastián Gutiérrez Valenciano <up220628@alumnos.upa.edu.mx>
2025-07-17 14:35:15 -0600

    poner eso

---
+++ backend/src/main/java/com/consulta/ConsultaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.consulta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultaBackendApplication.class, args);
+    }
+}
